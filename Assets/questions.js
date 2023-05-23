e1fde7a6d3344bcdfa7dfec3d261f729c6abe836
ltram16 <ltram16@gmail.com>
2023-05-22 22:17:40 -0700

    added questions js

---
+++ Assets/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        Question: "Where is the correct place to insert JavaScript?",
+        Choices: [
+             "The <body> section", "The <head> section", "Both the <head> section and the <body> section", "None of the above"],
+        Answer: "Both the <head> section and the <body> section",
+    },
+    {
+        Question: "String values must be enclosed within ______ when being assigned to variables?",
+        Choices: ["Commas", "Curly brackets", "Squared-brackets", "Parentheses"],
+        Answer: "Parentheses",
+    },
+    {
+        Question: "Arrays in JavaScript can be used to store ______",
+        Choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        Answer: "All of the above",
+    },
+    {
+        Question: "How do you call a function?",
+        Choices: ["call myFunction()", "myFunction()", "call function myFunction()", "All of the above"],
+        Answer: "myFunction()",
+    },
+    {
+        Question: "Inside which element do we put the JavaScript src?",
+        Choices: ["<javascript>", "<js>", "<scripting>", "<script>"],
+        Answer: "<script>", 
+    },
+];
